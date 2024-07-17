039e1577d323d4dd613e813b23f2d022b84d7c09
CSY2022 <tel19807034568@outlook.com>
2024-07-17 17:29:25 +0800

    暂时修改成这样

--- js/worker.js
+++ js/worker.js
@@ -1,25 +1,13 @@
-        function startTimer() {
+function startTimer() {
+  const startTime = localStorage.getItem('startTime');
+  const now = new Date().getTime();
+  const twentyMinutes = 20 * 60 * 1000; // 20 分钟的毫秒数
 
-            const startTime = localStorage.getItem('startTime');
-            const now = new Date().getTime();
-            const twentyMinutes = 0.1 * 60 * 1000;
-while(1){
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay)) 
-await sleep(500)
-            // 如果没有开始时间，设置当前时间为开始时间
-            if (!startTime) {
-                localStorage.setItem('startTime', now);
-            } else {
-                const elapsedTime = now - startTime;
-                if (elapsedTime >= twentyMinutes) {
-                    window.location.href = 'break.html';
-                } else {
-                    setTimeout(function() {
-                        window.location.href = 'break.html';
-                    }, twentyMinutes - elapsedTime);
-                }
-            }
-            SetTimeout () 
-            }
-        }
+  if (!startTime) {
+    localStorage.setItemcalStorage.getItem('startTime');
+  const now = new Date().getTime();
+  const twentyMinutes = 20 * 60 * 1000; // 20 分钟的毫秒数
+  // 如果没有开始时间，设置当前时间为开始时间
+  if (!startTime) {
+    localStorage.setItem
 startTimer();
