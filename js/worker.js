9f76a5a305fe04ff935cae00173f959f6e5260f9
CSY2022 <tel19807034568@outlook.com>
2024-07-17 22:54:09 +0800

    Fix bogs

--- js/worker.js
+++ js/worker.js
@@ -1,13 +1,20 @@
 function startTimer() {
-  const startTime = localStorage.getItem('startTime');
-  const now = new Date().getTime();
-  const twentyMinutes = 20 * 60 * 1000; // 20 分钟的毫秒数
+            const startTime = localStorage.getItem('startTime');
+            const now = new Date().getTime();
+            const limitTime = 20 * 60 * 1000;
 
-  if (!startTime) {
-    localStorage.setItemcalStorage.getItem('startTime');
-  const now = new Date().getTime();
-  const twentyMinutes = 20 * 60 * 1000; // 20 分钟的毫秒数
-  // 如果没有开始时间，设置当前时间为开始时间
-  if (!startTime) {
-    localStorage.setItem
+            // 如果没有开始时间，设置当前时间为开始时间
+            if (!startTime) {
+                localStorage.setItem('startTime', now);
+            } else {
+                const elapsedTime = now - startTime;
+                if (elapsedTime >= limitTime) {
+                    window.location.href = 'break.html';
+                } else {
+                    setTimeout(function() {
+                        window.location.href = 'break.html';
+                    }, limitTime - elapsedTime);
+                }
+            }
+}
 startTimer();
---
+++ js/worker2.js
@@ -0,0 +1,2 @@
+// 设置20分钟（1200000毫秒）后跳转到休息页面
+setTimeout(function() { window.location.href = 'break.html'; }, 1200000); // 20分钟 = 20 * 60 * 1000 毫秒
